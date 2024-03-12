708753124b67326c787d3b2e5b2c51da3cabe0e5
kingofcarrot <76939391@qq.com>
2024-03-12 10:00:10 +0800

    获取所有航空公司信息

--- src/main/java/com/scu/planeticket/mapper/AirlineInfoMapper.java
+++ src/main/java/com/scu/planeticket/mapper/AirlineInfoMapper.java
@@ -1,7 +1,10 @@
 package com.scu.planeticket.mapper;
 
+import com.scu.planeticket.pojo.dto.GetAirlineInfoListRespDTO;
 import com.scu.planeticket.pojo.entity.AirlineInfo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -12,5 +15,5 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2024-03-09
  */
 public interface AirlineInfoMapper extends BaseMapper<AirlineInfo> {
-
+    List<GetAirlineInfoListRespDTO> getInfoList();
 }
