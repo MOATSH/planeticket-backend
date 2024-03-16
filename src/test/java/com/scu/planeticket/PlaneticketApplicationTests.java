6517c566544719d85151e9046f79af432869940e
MOATSH <3593132199@qq.com>
2024-03-16 12:35:07 +0800

    配置修改

--- src/test/java/com/scu/planeticket/PlaneticketApplicationTests.java
+++ src/test/java/com/scu/planeticket/PlaneticketApplicationTests.java
@@ -1,13 +1,13 @@
-package com.scu.planeticket;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class PlaneticketApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.scu.planeticket;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class PlaneticketApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
